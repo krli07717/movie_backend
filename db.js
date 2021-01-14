1adbd602f3b97e217a0b6283bc568a52b09a8aea
kunjuili <k.r.li07717@gmail.com>
2021-01-14 21:09:42 +0800

    heroku db

--- db.js
+++ db.js
@@ -9,15 +9,4 @@ const pool = new Client({
 
 pool.connect();
 
-pool.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    pool.end();
-  }
-);
-
 module.exports = pool;
